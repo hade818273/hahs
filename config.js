5d532dbaa225837d4d8ce0e036c7fb2535984867
hade818273 <167134546+hade818273@users.noreply.github.com>
2024-04-16 15:54:34 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot: {
+    owners: ["948386383502901249"],  // ايدي الاونر
+    botID: "1035662460729958510",    // ايدي البوت
+    GuildId: "1081974415316746400",   // ايدي السيرفير
+    ClientId: "1035662460729958510",    // ايدي البوت
+    serverinvte: "https://discord.gg/YfJaGyPu5W", // انفايت سيرفر
+    clientSECRET: process.env.client , // سكريت
+    callbackURL: "https://18f29d12-da0f-448c-9f06-8dc8ab6a93fe-00-1d3t31uzvt6r2.picard.replit.dev/login", // الكال باك
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1035662460729958510&permissions=8&scope=bot",// انفايت البوت
+    TheLinkVerfy : 'https://discord.com/oauth2/authorize?client_id=1035662460729958510&response_type=code&redirect_uri=https%3A%2F%2F18f29d12-da0f-448c-9f06-8dc8ab6a93fe-00-1d3t31uzvt6r2.picard.replit.dev%2Flogin&scope=identify+email+guilds+guilds.join', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+    ceatogry : '1116853350009405512', // كاتوجري الي يفتح فيها تكت شراء
+     TOKEN: (process.env.midd),// توكن 
+    Price: 1,    // سعر العضو الواحد
+    TraId  : '948386383502901249' // ايدي الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
